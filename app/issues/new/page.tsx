6aa82b8356338c81640fd35dcc6d60481bbb1c13
Biswadeepgiri <biswadeepgiri48@gmail.com>
2023-12-26 23:32:14 +0530

    Implemented custom error handlings.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -5,12 +5,14 @@ import { TextArea } from '@radix-ui/themes';
 import { Button } from '@radix-ui/themes';
 import {useForm} from 'react-hook-form';
 import { Controller } from 'react-hook-form';
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Issue } from '@prisma/client';
 import { useRouter } from 'next/navigation';
+import { Callout } from '@radix-ui/themes';
+import { MdErrorOutline } from "react-icons/md";
 
 interface IssueForm {
   title: String,
@@ -22,12 +24,33 @@ const NewIssuePage = () => {
   const router = useRouter();
 
   const {register,control,handleSubmit} = useForm<IssueForm>();
+  const [err,setErr] = useState('');
   
   return (
-    <form className='max-w-xl space-y-3'
+    <div className='max-w-xl'>
+      {err && (
+      <Callout.Root color="red" className='mb-5'>
+        <Callout.Icon>
+    <MdErrorOutline />
+  </Callout.Icon>
+        <Callout.Text>{err}</Callout.Text>
+
+      </Callout.Root>
+      )
+}
+
+    
+
+    <form className=' space-y-3'
       onSubmit={handleSubmit(async (data) => {
+        try{
         await axios.post('/api/issues',data);
         router.push('/issues')
+        }
+        catch(error){
+          setErr('An unexpected error occured');
+
+        }
       })}>
         <TextField.Root>
             <TextField.Input placeholder='Tilte' {...register('title')} />
@@ -41,6 +64,7 @@ const NewIssuePage = () => {
         <Button>Submit New Issue</Button>
         
     </form>
+    </div>
   )
 }
 
